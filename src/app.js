e9c1b9f8a7418f22e7714a879dafc4bab2215ce5
Franchesca Barias <franchesca.barias7@gmail.com>
2022-09-04 14:02:53 +0100

    displayForecast function created and forEach function added for each day. Also,  HTML wtih forecast ID was integrated into the displayForecast function

--- src/app.js
+++ src/app.js
@@ -43,8 +43,8 @@ const apiKey = "d9a1ffbb13c0cf12ce582064cf96a4eb";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=48.8566&lon=2.3522&appid=${apiKey}&units=metric`;
 axios.get(apiUrl).then(showTemperature);
 
+
 function showTemperature(response) {
-  console.log(response.data);
   document.querySelector('#city').innerHTML = response.data.name;
   document.querySelector('#temperature').innerHTML = Math.round(response.data.main.temp);
   let descriptionElement = document.querySelector('#description');
@@ -52,12 +52,31 @@ function showTemperature(response) {
   let windElement = document.querySelector('#wind');
   let iconElement = document.querySelector('#current-icon');
 
+
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
-  iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+  iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`)
+  console.log(response.data);
 };
 
+function displayForecast() {
+  let forecastElement = document.querySelector('#forecast');
+
+  let forecastHTML = "";
+  let days = ["Mon", "Tue", "Wed", "Thu", "Fri"];
+  days.forEach(function (day) {
+    forecastHTML = forecastHTML + `
+      <div class="one">
+        <p id="day">${day}</p>
+        <img src="images/sun.gif" alt="sunny">
+        <p><span class="degrees">18℃</span></p>
+      </div>`
+  });
+  forecastElement.innerHTML = forecastHTML;
+};
+
+displayForecast();
 
 
 let fahrenheit = document.querySelector('#fahrenheit');
